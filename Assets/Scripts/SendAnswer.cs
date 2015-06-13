24ddd3b541bd9884318067ad788f40f35604660e
hiroc0922 <hiroc0922@gmail.com>
2015-06-14 07:51:52 +0900

    sendのボタン作成

---
+++ Assets/Scripts/SendAnswer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SendAnswer : MonoBehaviour {
+
+
+public GameObject TextBox;
+private string AnswerText;
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	
+	public void onClick(){
+        
+    }
+	
+	
+}
