adc5dce53730a74a331a5ea68df75b617b6da8ed
hiroc0922 <hiroc0922@gmail.com>
2015-06-14 08:04:58 +0900

    top画面作成

---
+++ Assets/Scripts/top.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class top : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void onClick(){
+
+	}
+
+}
